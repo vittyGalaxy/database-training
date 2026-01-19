0757195fd3d9fe4e1ee878ce18cd010694d416be
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-01-19 23:29:33 +0100

    feat: implemented query

--- sql/dipendenti/query_by_vitto.sql
+++ sql/dipendenti/query_by_vitto.sql
@@ -1,50 +1,69 @@
--- elenco film completo
-SELECT *
-FROM film
+-- 1. visualizzare l'elenco dei film
+select *
+from dipendenti
 
--- elenco di film del 1998
-SELECT *
-FROM film
-WHERE annoproduzione = 1998
+-- 2. visualizzare i dipendenti nati a Roma e quelli nati a Torino
+select *
+from dipendenti
+where comunenascita = "Roma" or comunenascita = "Torino"
 
--- elenco di film dal 2007 in poi in ordine di costo
-SELECT *
-FROM film
-WHERE annoproduzione >= 2007
-ORDER BY costo ASC
+-- 3. visualizzare i dipendenti che hanno dai 4 ai 7 figli in ordine alfabetico
+select *
+from dipendenti
+where numfigli >= 4 and numfigli <= 7
 
--- elenco di film prodotti dal 2000 fino al 2010 escluso
-SELECT *
-FROM film
-WHERE annoproduzione >= 2000 AND annoproduzione < 2010
+-- 4. visualizzare i dipendenti con stipendio di almeno 1000 euro e meno di 3 figli in ordine di stipendio dallo stipendio più alto al più basso
+select *
+from dipendenti
+where stipendio >= 1000 and numfigli < 3
+order by stipendio desc
 
--- elenco di film esclusi dal 2000 al 2005
-SELECT *
-FROM film
-WHERE annoproduzione < 2000 OR annoproduzione > 2005
+-- 5. visualizzare i dipendenti nati a Roma, Bari, Napoli
+select *
+from dipendenti
+where comunenascita = "Roma" or comunenascita = "Bari" or comunenascita = "Napoli"
 
--- elenco di film dopo il 2007 costati meno di 10000 euro
-SELECT *
-FROM film
-WHERE annoproduzione > 2007 and costo < 10000
+-- 6. visualizzare la media degli stipendi dei dipendenti
+select AVG(stipendio) as media_stipendi
+from dipendenti
 
--- elenco di film del regista "Gabriele Muccino"
-SELECT *
-FROM film
-WHERE nomeregista = "Gabriele" and cognomeregista = "Muccino"
+-- 7. visualizzare lo stipendio minimo e lo stipendio massimo
+select min(stipendio) as minimo,
+       max(stipendio) as massimo
+from dipendenti
 
--- elenco di film che come nome autore é "alessandro" e italiano
-SELECT *
-FROM film
-WHERE nomeregista = "alessandro" and nazione = "Italia"
+-- 8. visualizzare il dipendente che ha lo stipendio minore
+select *
+from dipendenti
+where stipendio = (select min(stipendio) from dipendenti)
 
--- elenco di film italiano e francese
-SELECT *
-FROM film
-WHERE nazione = "Italia" OR nazione = "Francia"
+-- 9. quanti dipendenti non hanno figli?
+select *
+from dipendenti
+where numfigli = 0
 
--- elenco di film del 2007 ordinati per nazione in maniera decrescente
-SELECT *
-FROM film
-WHERE annoproduzione = 2007
-ORDER BY nazione DESC
+-- 10. in quanti dipendenti non ho inserito il numero di figli?
+select count(*) as senza_figli
+from dipendenti
+where numfigli is null
+
+-- 11. chi sono i dipendenti in cui non ho inserito il numero di figli?
+select iddip, nome, cogn
+from dipendenti
+where numfigli is null
+
+-- 12. quali dipendenti hanno il golf tra i propri hobby?
+select *
+from dipendenti
+where hobby like "%golf%"
+
+-- 13. quali dipendenti hanno il golf e la pallacanestro tra i propri hobby?
+select *
+from dipendenti
+where hobby like "%golf%" and
+      hobby like "%pallacanestro%"
+
+-- 14. quanti dipendenti ascoltano musica classica?
+select count(*) as ascoltano_musica_classica
+from dipendenti
+where hobby like "%musica classica%"

787cfad0c3fc2e6c66120bcba59d56b4800487a2
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-01-21 00:21:53 +0100

    feat: implemented new query

--- sql/dipendenti/query_by_vitto.sql
+++ sql/dipendenti/query_by_vitto.sql
@@ -67,3 +67,20 @@ where hobby like "%golf%" and
 select count(*) as ascoltano_musica_classica
 from dipendenti
 where hobby like "%musica classica%"
+
+-- 15. trovare il dipendente in cui ho inserito per errore la parola pallacanestro tra gli hobby in cui ho probabilmente sbagliato l'ultima lettera
+select *
+from dipendenti
+where hobby LIKE '%pallacanestr_%'
+  AND hobby NOT LIKE '%pallacanestro%';
+
+-- 16. visualizzare i dipendenti che hanno al massimo 500 euro di stipendio e almeno 4 figli con lo stipendio aumentato di 350 euro (bonus famiglia), visualizzando anche lo stipendio iniziale
+select iddip,
+       nome,
+       cogn,
+       stipendio as stipendio_no_famiglia,
+       stipendio + 350 as stipendio_famiglia,
+       numfigli
+from dipendenti
+where stipendio <= 500 and
+      numfigli >= 4

53b22f9166c3fedf1fb254665f0abd39f12a2fe4
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-02-26 19:02:10 +0100

    feat: implemented query number 4

--- sql/orologi/query.sql
+++ sql/orologi/query.sql
@@ -1,6 +1,6 @@
 -- Active: 1768521664416@@127.0.0.1@3306@esercizi
 -- 1. visualizzare l'elenco di tutti gli orologi (mostrare tipo e produttore)
-select orologi.idorologio, orologi.nome, orologi.valore, orologi.descrizione, produttori.nomeproduttore, tipi.tipo
+select orologi.idorologio, orologi.idorologio, orologi.nome, orologi.valore, orologi.descrizione, produttori.nomeproduttore, tipi.tipo
 from orologi
 join produttori on orologi.idproduttore = produttori.idproduttore
 join tipi on orologi.idtipo = tipi.idtipo
@@ -12,8 +12,16 @@ join tipi on orologi.idtipo = tipi.idtipo
 where orologi.nome like "Breil%"
 
 -- 3. visualizzare l'elenco di tutti gli orologi della Breil il cui valore supera i 350 euro
-select orologi.idorologio, orologi.nome, orologi.valore, orologi.descrizione, tipi.tipo
+select orologi.idorologio, orologi.idorologio, orologi.nome, orologi.valore, orologi.descrizione, tipi.tipo
 from orologi
 join tipi on orologi.idtipo = tipi.idtipo
-where orologi.nome like "Breil%" and
+where   orologi.nome like "Breil%" and
         orologi.valore > 350
+
+-- 4. visualizzare l'elenco di tutti gli orologi il cui valore va tra i 200 e i 400 euro
+select orologi.idorologio, orologi.idorologio, orologi.nome, orologi.valore, orologi.descrizione, tipi.tipo
+from orologi
+join produttori on orologi.idproduttore = produttori.idproduttore
+join tipi on orologi.idtipo = tipi.idtipo
+where   orologi.valore >= 200 and
+        orologi.valore <= 400

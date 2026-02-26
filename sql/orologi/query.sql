575ecd596ede4bfc2167b7c0c26deb7ba02b8743
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-02-26 18:57:03 +0100

    feat:  implemented query number 3

--- sql/orologi/query.sql
+++ sql/orologi/query.sql
@@ -10,3 +10,10 @@ select orologi.idorologio, orologi.nome, orologi.valore, orologi.descrizione, ti
 from orologi
 join tipi on orologi.idtipo = tipi.idtipo
 where orologi.nome like "Breil%"
+
+-- 3. visualizzare l'elenco di tutti gli orologi della Breil il cui valore supera i 350 euro
+select orologi.idorologio, orologi.nome, orologi.valore, orologi.descrizione, tipi.tipo
+from orologi
+join tipi on orologi.idtipo = tipi.idtipo
+where orologi.nome like "Breil%" and
+        orologi.valore > 350
